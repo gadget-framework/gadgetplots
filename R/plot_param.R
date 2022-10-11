2f51317f404a8abaeb48860eca9cbd5273200532
MikkoVihtakari <mikko.vihtakari@hi.no>
2022-10-11 08:57:49 +0200

    Add plot_param to make_html optionally

--- R/plot_param.R
+++ R/plot_param.R
@@ -7,31 +7,36 @@
 
 plot_param <- function(fit, out_only = FALSE, base_size = 8) {
 
-  dat <- fit$params %>%
-    dplyr::filter(.data$optimise == 1) %>%
-    dplyr::mutate(rho = (.data$value-.data$lower)/(.data$upper - .data$lower))
+  if(all(c("optimise", "lower", "upper") %in% names(fit$params))) {
+    dat <- fit$params %>%
+      dplyr::filter(.data$optimise == 1) %>%
+      dplyr::mutate(rho = (.data$value-.data$lower)/(.data$upper - .data$lower))
 
-  if(out_only) dat <- dat %>% dplyr::filter(.data$rho < 0 | .data$rho > 1)
+    if(out_only) dat <- dat %>% dplyr::filter(.data$rho < 0 | .data$rho > 1)
 
-  if(nrow(dat) > 0) {
-    suppressWarnings({
-      ggplot2::ggplot(
-        data = dat,
-        ggplot2::aes(.data$switch,.data$rho,label=.data$switch,
-                     text = paste("value :", round(.data$value)))) +
-        ggplot2::geom_point() +
+    if(nrow(dat) > 0) {
+      suppressWarnings({
+        ggplot2::ggplot(
+          data = dat,
+          ggplot2::aes(.data$switch,.data$rho,label=.data$switch,
+                       text = paste("value :", round(.data$value)))) +
+          ggplot2::geom_point() +
+          ggplot2::coord_flip() +
+          ggplot2::labs(x = "", y = "Rho") +
+          ggplot2::geom_hline(yintercept = c(0,1)) +
+          ggplot2::theme_bw(base_size = base_size)
+      })
+    } else {
+
+      ggplot2::ggplot() +
+        ggplot2::annotate("text", x = 1, y = 1, label = paste0(ifelse(out_only, "No parameters outside\ntheir boundaries", "No parameters to plot"))) +
         ggplot2::coord_flip() +
         ggplot2::labs(x = "", y = "Rho") +
-        ggplot2::geom_hline(yintercept = c(0,1)) +
-        ggplot2::theme_bw(base_size = base_size)
-    })
+        ggplot2::theme_bw(base_size = base_size) +
+        ggplot2::theme(axis.text = ggplot2::element_blank())
+    }
   } else {
-
-    ggplot2::ggplot() +
-      ggplot2::annotate("text", x = 1, y = 1, label = paste0(ifelse(out_only, "No parameters outside\ntheir boundaries", "No parameters to plot"))) +
-      ggplot2::coord_flip() +
-      ggplot2::labs(x = "", y = "Rho") +
-      ggplot2::theme_bw(base_size = base_size) +
-      ggplot2::theme(axis.text = ggplot2::element_blank())
+    warning("Did not find optimise, lower and upper columns in fit$param. Bug in g3_fit?")
+    return(NULL)
   }
 }
