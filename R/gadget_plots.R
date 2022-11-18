10e0d365ad0ffd9ba8b2e216e4678ae11f7ed6f8
will <will.butler@hafogvatn.is>
2022-11-18 11:37:47 +0000

    Do not try plotting stockdist if it doesn't exist

--- R/gadget_plots.R
+++ R/gadget_plots.R
@@ -78,28 +78,35 @@ gadget_plots <- function(fit, path, file_type = "png", quiet = FALSE, width = NU
     })
 
     ## Stock distribution
-    if(!quiet) message("Plotting stock distribution")
-
-    # tmp <- lapply(seq_along(tmp), function(i) {
-    #   tmp[[i]] +
-    #     ggplot2::ggtitle(names(tmp)[i]) +
-    #     ggplot2::expand_limits(y = 1) +
-    #     ggplot2::theme(legend.position = "none")
-    # })
-
-    ggplot2::ggsave(
-      file = paste0("Stockdist.", file_type),
-      plot = print(plot_stockdist(fit, stocks = "separate")),
-      path = path, bg = "white", width = width, height = height*2, units = units,
-      dpi = res)
-
-    ggplot2::ggsave(
-      file = paste0("Stockcomp.", file_type),
-      plot = print(
-        plot_stockdist(fit, type = "stock_composition",
-                       geom_area = TRUE)),
-      path = path, bg = "white", width = width, height = height*2, units = units,
-      dpi = res)
+    if (!is.null(fit$stockdist)){
+      
+      if(!quiet) message("Plotting stock distribution")
+      
+      # tmp <- lapply(seq_along(tmp), function(i) {
+      #   tmp[[i]] +
+      #     ggplot2::ggtitle(names(tmp)[i]) +
+      #     ggplot2::expand_limits(y = 1) +
+      #     ggplot2::theme(legend.position = "none")
+      # })
+      
+      ggplot2::ggsave(
+        file = paste0("Stockdist.", file_type),
+        plot = print(plot_stockdist(fit, stocks = "separate")),
+        path = path, bg = "white", width = width, height = height*2, units = units,
+        dpi = res)
+      
+      ggplot2::ggsave(
+        file = paste0("Stockcomp.", file_type),
+        plot = print(
+          plot_stockdist(fit, type = "stock_composition",
+                         geom_area = TRUE)),
+        path = path, bg = "white", width = width, height = height*2, units = units,
+        dpi = res) 
+      
+    }
+    else{
+      if (!quiet) message("No stockdist data to plot")
+    }
 
     ## Suitability
     if(!quiet) message("Plotting suitability")
