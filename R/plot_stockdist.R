9d09f2471cb87091df5345fc492afe811e099348
MikkoVihtakari <mikko.vihtakari@hi.no>
2023-02-01 06:55:03 +0100

    Try fixing those stockdist plots

--- R/plot_stockdist.R
+++ R/plot_stockdist.R
@@ -2,15 +2,20 @@
 #' @description Plots proportions of stocks by length or age in \code{fit$stockdist}
 #' @inheritParams plot_annual
 #' @param stocks Character vector specifying the stock to plot in \code{fit}. If \code{NULL}, all stocks are plotted in a single plot. If \code{"separate"}, all stocks are plotted in separate plots.
-#' @param component_name Character vector specifying the likelihood component to plot in \code{unique(fit$stockdist$name)}. If \code{NULL}, all components are plotted as a list. .
+#' @param component_name Character vector specifying the likelihood component to plot in \code{unique(fit$stockdist$name)}. If \code{NULL}, all components are plotted as a list.
+#' @param type Character specifying the plot type: \code{"step"} uses stepwise lines to show the aggregation intervals. \code{"line"} uses lines through average values between lower and upper intervals. If you want to show the aggregation intervals and to use average values (i.e. smooth lines), use the \code{show_intervals} argument.
+#' @param show_intervals Logical indicating whether length intervals should be plotted.
 #' @param color_palette A function defining the color palette or a vector of colors to be used for stocks.
 #' @details Plots model fit to data using likelihoods. Separate plot for each \code{unique(fit$stockdist$name)}.
 #' @return A \link[ggplot2]{ggplot} object or a list of such objects depending on the \code{stocks} argument.
 #' @export
 
 # Dev params
-# stocks = NULL; component_name = NULL; color_palette = scales::hue_pal(); base_size = 8
-plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_palette = scales::hue_pal(), base_size = 8) {
+# stocks = NULL; component_name = NULL; type = "step"; show_intervals = FALSE; color_palette = scales::hue_pal(); base_size = 8
+plot_stockdist <- function(
+    fit, stocks = NULL, component_name = NULL, type = "step", show_intervals = FALSE,
+    color_palette = scales::hue_pal(), base_size = 8
+) {
 
   ## Check whether fit contains stockdist
   if (is.null(fit$stockdist)) {
@@ -18,6 +23,23 @@ plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_pale
     return(NULL)
   }
 
+  ## Old versions of g3_fit objects may not contain lower and upper columns
+
+  if(!all(c("lower", "upper") %in% names(fit$stockdist)) & type == "step") {
+    warning("The fit object does not contain lower and upper columns.
+            Was it made with an older version of gadgetutils perhaps?
+            Switching type to 'line'")
+    type <- "line"
+  }
+
+  if(!all(c("lower", "upper") %in% names(fit$stockdist)) & show_intervals) {
+    warning("The fit object does not contain lower and upper columns.
+            Was it made with an older version of gadgetutils perhaps?
+            Switching type to show_intervals to FALSE")
+    show_intervals <- FALSE
+  }
+
+  ## Account for the stock or stock_re columns
   all_stocks <- c(stats::na.exclude(unique(fit$stockdist$stock)))
   if(!is.null(fit$stockdist$stock_re)) {
     all_stock_res <- rev(c(stats::na.exclude(unique(fit$stockdist$stock_re))))
@@ -25,6 +47,7 @@ plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_pale
     all_stock_res <- NULL
   }
 
+  ## Colors
   if(inherits(color_palette, "function")) {
 
     cols <- stats::setNames(color_palette(length(c(all_stocks, all_stock_res))),
@@ -37,35 +60,83 @@ plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_pale
     }
   }
 
-  lenplot <- function(stocks = NULL, stockdist_name = component_name, color_palette) {
+  ## Plot for length distributions
+  lenplot <- function(stocks = NULL, stockdist_name = component_name, type = type, color_palette) {
 
-  if(is.null(stockdist_name)) stockdist_name <- unique(fit$stockdist$name)[1]
+    if(is.null(stockdist_name)) stockdist_name <- unique(fit$stockdist$name)[1]
 
-      x <- fit$stockdist %>%
-        #dplyr::filter(.data$predicted > 1e-4) %>%
-        dplyr::filter(.data$name == stockdist_name) %>%
-        dplyr::mutate(
-          pred.ratio =
-            ifelse(.data$pred.ratio == 0, NA,
-                   ifelse(is.nan(.data$pred.ratio), 0, .data$pred.ratio)),
-          obs.ratio = ifelse(is.na(.data$pred.ratio), NA, .data$obs.ratio)
-        )
+    x <- fit$stockdist %>%
+      dplyr::filter(.data$name == stockdist_name) %>%
+      dplyr::mutate(
+        pred.ratio =
+          ifelse(.data$pred.ratio == 0, NA,
+                 ifelse(is.nan(.data$pred.ratio), 0, .data$pred.ratio)),
+        obs.ratio = ifelse(is.na(.data$pred.ratio), NA, .data$obs.ratio)
+      ) # %>%
+    # dplyr::group_by(.data$year, .data$step, .data$length) %>%
+    # dplyr::mutate(n = sum(.data$observed))
 
-      if (all(is.na(x$stock_re)) & all(!is.na(x$stock))) {
-        if(is.null(stocks)) stocks <- c(stats::na.exclude(unique(fit$stockdist$stock)))
-        x <- x %>% dplyr::filter(.data$stock %in% stocks)
-        stock_label <- "Stock"
-      } else if (all(!is.na(x$stock_re)) & all(is.na(x$stock))) {
-        if(is.null(stocks)) stocks <- c(stats::na.exclude(unique(fit$stockdist$stock_re)))
-        x <- x %>%
-          dplyr::filter(.data$stock_re %in% stocks) %>%
-          dplyr::select(-.data$stock) %>%
-          dplyr::rename("stock" = "stock_re")
-        stock_label <- "Stock component"
-      } else {
-        stop("Both stock and stock_re in the same data. Don't know what to do.")
-      }
+    # x[x$predicted < 1e-3,]$pred.ratio <- NA
 
+    ## Find whether data use stock or stock_re column
+    if (all(is.na(x$stock_re)) & all(!is.na(x$stock))) {
+      if(is.null(stocks)) stocks <- c(stats::na.exclude(unique(fit$stockdist$stock)))
+      x <- x %>% dplyr::filter(.data$stock %in% stocks)
+      stock_label <- "Stock"
+    } else if (all(!is.na(x$stock_re)) & all(is.na(x$stock))) {
+      if(is.null(stocks)) stocks <- c(stats::na.exclude(unique(fit$stockdist$stock_re)))
+      x <- x %>%
+        dplyr::filter(.data$stock_re %in% stocks) %>%
+        dplyr::select(-.data$stock) %>%
+        dplyr::rename("stock" = "stock_re")
+      stock_label <- "Stock component"
+    } else {
+      stop("Both stock and stock_re in the same data. Don't know what to do.")
+    }
+
+    if(show_intervals) {
+      length_groups <- c(x$lower, x$upper) %>% unique() %>% sort()
+    }
+
+    if(type == "step") {
+
+      x <- x %>%
+        dplyr::select(.data$year, .data$step, .data$stock, .data$lower,
+                      .data$upper, .data$obs.ratio, .data$pred.ratio)
+
+      x <- x %>% dplyr::select(-.data$upper) %>%
+        dplyr::bind_rows(
+          x %>%
+            dplyr::filter(.data$upper == max(.data$upper)) %>%
+            dplyr::select(-.data$lower) %>%
+            dplyr::rename("lower"= "upper")
+        ) %>%
+        dplyr::arrange(.data$year, .data$step, .data$stock, .data$lower)
+
+
+      ### Plot
+      suppressWarnings({
+        ggplot2::ggplot(x) + {
+          if(show_intervals) ggplot2::geom_vline(
+            xintercept = length_groups, color = "grey", linetype = "dotted")
+        } +
+          ggplot2::geom_step(
+            ggplot2::aes(x = .data$lower, y = .data$obs.ratio, lty = .data$stock),
+            color = "grey") +
+          ggplot2::geom_step(
+            ggplot2::aes(x = .data$lower, y = .data$pred.ratio, lty = .data$stock),
+            color = "black") +
+          ggplot2::facet_wrap(~.data$year+.data$step,
+                              labeller = ggplot2::label_wrap_gen(multi_line=FALSE)) +
+          ggplot2::expand_limits(y = c(0,1)) +
+          ggplot2::scale_y_continuous(breaks = seq(0,1,0.25)) +
+          ggplot2::labs(y = 'Stock proportion', x = 'Length', lty = stock_label) +
+          # ggplot2::scale_color_manual(values = cols) +
+          ggplot2::theme_classic(base_size = base_size) +
+          ggplot2::theme(legend.position = "bottom",
+                         strip.background = ggplot2::element_blank())
+      })
+    } else {
       x %>%
         ggplot2::ggplot(
           ggplot2::aes(x = .data$length, y = .data$obs.ratio,
@@ -83,8 +154,11 @@ plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_pale
         ggplot2::theme_classic(base_size = base_size) +
         ggplot2::theme(legend.position = "bottom",
                        strip.background = ggplot2::element_blank())
+    }
+
   }
 
+  ## Plot for age distributions
   lenageplot <- function(x) {
     x %>%
       dplyr::mutate(pred.ratio =
@@ -124,7 +198,9 @@ plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_pale
 
         if(is.null(stocks)) {
 
-          lenplot(stocks = NULL, stockdist_name = x, color_palette = cols)
+          suppressWarnings({
+            lenplot(stocks = NULL, stockdist_name = x, type = type, color_palette = cols)
+          })
 
         } else {
 
@@ -142,13 +218,16 @@ plot_stockdist <- function(fit, stocks = NULL, component_name = NULL, color_pale
           #   lenplot(stocks = stocks, stockdist_name = x, color_palette = cols))
 
           #cowplot::plot_grid(
-            cowplot::plot_grid(
-              plotlist = lapply(stocks, function(k) {
-                lenplot(stocks = k, stockdist_name = x, color_palette = cols) +
+          cowplot::plot_grid(
+            plotlist = lapply(stocks, function(k) {
+              suppressWarnings({
+                lenplot(stocks = k, stockdist_name = x, type = type,
+                        color_palette = cols) +
                   ggplot2::theme(legend.position = "none") +
                   ggplot2::ggtitle(k)
               })
-           # ), legend, ncol = 1, rel_heights = c(1, .1)
+            })
+            # ), legend, ncol = 1, rel_heights = c(1, .1)
           )
         }
       } else {
