2dd8a788b3a3f4d2ceb4d0a82a242f7e02385ae3
MikkoVihtakari <mikko.vihtakari@hi.no>
2025-08-21 14:40:32 +0200

    Fix a bug introduced by changes to g3_fit

--- R/plot_weight.R
+++ R/plot_weight.R
@@ -9,18 +9,27 @@
 #' @export
 
 plot_weight <- function(fit, log_scale = FALSE, base_size = 8) {
-
+  
   if (is.list(fit$params$value)) fit$params$value <- unlist(fit$params$value)
-
-  dat <- fit$params %>%
-    dplyr::filter(grepl("weight$", .data$switch)) %>%
-    dplyr::mutate(switch = gsub("_weight$", "", .data$switch))
-
+  
+  if(is.list(fit$params)) {
+    tmp <- unlist(fit$params[grepl("weight$", names(fit$params))])
+    
+    dat <- setNames(as.data.frame(tmp), "value") %>% 
+      mutate(switch = names(tmp), .before = 1) %>% 
+      dplyr::mutate(switch = gsub("_weight$", "", .data$switch))
+    
+  } else {
+    dat <- fit$params %>%
+      dplyr::filter(grepl("weight$", .data$switch)) %>%
+      dplyr::mutate(switch = gsub("_weight$", "", .data$switch))
+  }
+  
   if(all(dat$value %in% c(1, 0)) & log_scale) {
     message("Weights are only 1s and 0s. Setting log_scale = FALSE")
     log_scale <- FALSE
   }
-
+  
   dat %>%
     ggplot2::ggplot(ggplot2::aes(x = .data$switch, y = .data$value)) +
     ggplot2::geom_col() +
@@ -29,5 +38,5 @@ plot_weight <- function(fit, log_scale = FALSE, base_size = 8) {
       if(log_scale) ggplot2::scale_y_log10()
     } +
     ggplot2::theme_bw(base_size = base_size)
-
+  
 }
